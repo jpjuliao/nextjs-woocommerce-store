07f8a7dd0c312539152e9bf5b6fb95a037666872
JP Juliao <jpjuliao@gmail.com>
2024-10-07 19:16:56 -0500

    Add index and cart page

---
+++ src/pages/cart.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from '@/components/Layout';
+import CartDetail from '@/components/CartDetail';
+
+/**
+ * The CartPage component wraps the CartDetail in the Layout.
+ */
+const CartPage: React.FC = () => {
+  return (<Layout>
+    <h1 className="text-3xl font-bold mb-6">Your Cart</h1>
+    <CartDetail />
+  </Layout>
+  );
+};
+
+export default CartPage;
